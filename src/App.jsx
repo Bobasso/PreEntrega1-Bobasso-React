2c9c7607ec99b8c4761107a5c11c98b2e0991aeb
Bobasso <mbobasso86@gmail.com>
2024-10-06 20:29:12 -0300

    cambios 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 
 function App() {
-
   return (
     <div className='container-app'>
       <NavBar/>
