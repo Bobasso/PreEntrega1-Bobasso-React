15b7f8e6cbc939289f82b5756157c76749b220ca
Bobasso <mbobasso86@gmail.com>
2024-10-10 13:40:09 -0300

    creo footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <div className='container-app'>
+    <div className='min-h-screen flex flex-col'>
       <NavBar/>
       <ItemListContainer saludo={"Filtros"}/>
       <ItemListContainer saludo={"Este es mi ecommerce"}/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center p-2 bg-gray-100 w-full">
+        <p className="text-sm text-gray-800 text-opacity-70">© 2024 Ropa Sin Fronter, Inc. Derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer
