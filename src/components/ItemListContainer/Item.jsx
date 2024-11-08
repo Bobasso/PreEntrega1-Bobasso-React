4fc78d03cf33ff559bc0eefc185b22a1038a95a5
Bobasso <mbobasso86@gmail.com>
2024-11-08 19:52:25 -0300

    uso memo

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,6 +1,7 @@
+import React from "react"
 import { Link } from "react-router-dom"
 
-const Item = ({ product }) => {
+const Item = React.memo( ({ product }) => {
   return (
     <Link to={"/product/" + product.id} className="mx-auto my-4 h-auto rounded-lg shadow-inner bg-gray-50 transition ease-in-out duration-500 hover:scale-110 hover:cursor-pointer">
         <img src={product.image} alt={product.name} className="h-36 w-36 sm:h-48 sm:w-48 px-2 py-2 border-b border-gray-500"/>
@@ -8,6 +9,6 @@ const Item = ({ product }) => {
         <p>{product.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</p>
     </Link>
   )
-}
+}, (prevProps, nextProps) => JSON.stringify(prevProps) === JSON.stringify(nextProps) )
 
 export default Item
